4a7abc9ece5aaa8c3a9ea9a761d8b66685e896a3
Nigel Faria <nigel.faria@gmail.com>
2023-02-14 22:45:33 +0000

    feat:css highlighting when nav is active

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -2,12 +2,12 @@ import { NavLink } from "react-router-dom";
 export const Header = () => {
     return (
         <header className="header">
-            <div className="header__logo">Fakelandia</div>
+            <div className="header__logo">Fakelandia Justice Department</div>
             <nav className="header__nav">
                 <ul className="header__ul">
                     <li className="header__nav-item"><NavLink to='/'>Home</NavLink></li>
-                    <li className="header__nav-item"><NavLink to='/confession'>Confession</NavLink></li>
-                    <li className="header__nav-item"><NavLink to='/misdemeanour'>Misdemeanour</NavLink></li>
+                    <li className="header__nav-item"><NavLink to='/misdemeanour'>Misdemeanours</NavLink></li>
+                    <li className="header__nav-item"><NavLink to='/confession'>Confess To Us</NavLink></li>
                 </ul>
             </nav>
 
